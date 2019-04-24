c665583d3c758be439a5aeb87d41e8c30de44fcf
Sergey home <po4ta56@mail.ru>
2019-04-24 23:58:21 +0500

    registration template

--- site_for_tasks/urls.py
+++ site_for_tasks/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls'))
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
