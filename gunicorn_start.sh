a60a5c51bea7a7986d46f5182e1658bbd1e0345d
Sergey Fomin <po4ta56@mail.ru>
2019-05-08 10:52:38 +0500

    add start nginx

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/site-for-tasks
-gunicorn --workers=5 -b :8080  site_for_tasks.wsgi:application &
+service nginx start
+gunicorn --workers=5 -b :8080  site_for_tasks.wsgi:application 
